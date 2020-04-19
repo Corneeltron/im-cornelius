998ffbea8b9c2230878a9f84df0327e198c25322
cornelius phanthanh <corneliusphanthanh@gmail.com>
2020-04-19 16:09:24 -0700

    Added components and styles

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../components/header";
+
+export default () => (
+	<div style={{ color: `teal` }}>
+		<Header headerText="About Corneeltron"/>
+		<p>Born in Germany to Vietnamese parents, raised in California, lived in Japan for 6 years and New Zealand for one. Applying communication skills from a multicultural upbringing to the online world. Javascript, React/Redux, NodeJS/Express, MongoDB, PostGreSQL, and Graphic design.</p>
+	</div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+	<div style={{ color: `blue` }}>
+	<h1>Hi, I'm Cornelius.</h1>
+	<p>I'm a full-stack developer.</p>
+	</div>
+)
